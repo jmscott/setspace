c0258390a7a83949e0016fb9fb5d79bdcb9ce7dc
John the Scott <jmscott@setspace.com>
2015-10-16 17:58:31 -0500

    renamed schema from json to jsonorg

--- schema/jsonorg/schema.sql
+++ schema/jsonorg/schema.sql
@@ -9,46 +9,57 @@
 \timing
 
 begin;
-drop schema if exists json cascade;
+drop schema if exists jsonorg cascade;
 
-create schema json;
+create schema jsonorg;
 
-set search_path to json,public;
+set search_path to jsonorg,public;
 
 /*
  *  Is the blob valid json, max depth 255, according to the checker at
  *
  *	http://www.json.org/JSON_checker/
  */
-create table json.checker_255
+create table jsonorg.checker_255
 (
 	blob	udig
-			references setspace.blob(id)
+			references setspace.service(blob)
 			on delete cascade
 			primary key,
 	is_json	bool
 			not null
 );
+comment on table jsonorg.checker_255 is
+  'The json blob passes the checker at json.org, with max nesting of 255 levels'
+;
 
-create table json.jsonb_255
+/*
+ *  Binary json document that passes the json.org checker.
+ *
+ *  If you want the text of the json, just fetch the immutable blob.
+ */
+create table jsonorg.jsonb_255
 (
 	blob	udig
-			references json.checker_255
+			references jsonorg.checker_255
 			on delete cascade
 			primary key,
 	doc	jsonb
 			not null
 );
+comment on table jsonorg.jsonb_255 is
+	'Binary encoding of valid json document from table checker_255'
+;
 
 create index idx_jsonb_255_gin
   on
-  	json.jsonb_255 using gin (doc)
+  	jsonorg.jsonb_255 using gin (doc)
 ;
 
 --  for indexing the @> operator
 create index idx_jsonb_255_pgin
   on
-  	json.jsonb_255 using gin (doc jsonb_path_ops)
+  	jsonorg.jsonb_255 using gin (doc jsonb_path_ops)
 ;
 
 commit;
