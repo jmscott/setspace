48fab0b019e6c70d87ede03622cd5b07464a2490
John the Scott <jmscott@setspace.com>
2015-10-31 21:00:35 -0500

    added recipe for jsonb_255

--- schema/jsonorg/schema.sql
+++ schema/jsonorg/schema.sql
@@ -1,65 +1,109 @@
 /*
  *  Synopsis:
- *	Classifiy blobs fetched via blobio protocol.
+ *	Classifiy json as defined by the checker at json.org.
  *  Blame:
  *  	jmscott@setspace.com
- *  	setspace@gmail.com
+ *  Note:
+ *	Having problems revoking update privileges for owner.
+ *	alter default seems to fail, as well.
  */
 \set ON_ERROR_STOP on
 \timing
 
-begin;
-drop schema if exists jsonorg cascade;
+BEGIN;
+DROP SCHEMA IF EXISTS jsonorg cascade;
 
-create schema jsonorg;
+CREATE SCHEMA JSONORG;
 
-set search_path to jsonorg,public;
+ALTER DEFAULT PRIVILEGES FOR ROLE jmscott IN SCHEMA
+	jsonorg
+  REVOKE UPDATE ON TABLES FROM jmscott;
+;
 
 /*
  *  Is the blob valid json, max depth 255, according to the checker at
  *
  *	http://www.json.org/JSON_checker/
  */
-create table jsonorg.checker_255
+CREATE TABLE jsonorg.checker_255
 (
 	blob	udig
-			references setspace.service(blob)
-			on delete cascade
-			primary key,
+			REFERENCES setspace.service(blob)
+			ON DELETE CASCADE
+			PRIMARY KEY,
 	is_json	bool
 			not null
 );
-comment on table jsonorg.checker_255 is
+COMMENT ON TABLE jsonorg.checker_255 IS
   'The json blob passes the checker at json.org, with max nesting of 255 levels'
 ;
 
 /*
  *  Binary json document that passes the json.org checker.
  *
- *  If you want the text of the json, just fetch the immutable blob.
+ *  If you want the exact text of the json document
+ *  then just fetch the immutable blob.
  */
-create table jsonorg.jsonb_255
+CREATE TABLE jsonorg.jsonb_255
 (
 	blob	udig
-			references jsonorg.checker_255
-			on delete cascade
-			primary key,
+			REFERENCES jsonorg.checker_255
+			ON DELETE CASCADE
+			PRIMARY KEY,
 	doc	jsonb
-			not null
+			NOT NULL
 );
-comment on table jsonorg.jsonb_255 is
+
+CREATE OR REPLACE FUNCTION jsonorg.check_jsonability() RETURNS TRIGGER
+  AS $$
+	DECLARE
+		my_blob udig;
+	BEGIN
+
+	SELECT
+		blob into my_blob
+	  FROM
+	  	jsonorg.checker_255
+	  WHERE
+	  	blob = new.blob
+		and
+		is_json is true
+	;
+
+	IF FOUND THEN
+		RETURN new;
+	END IF;
+
+	RAISE EXCEPTION
+		'blob is not json: %', new.blob
+	USING
+		ERRCODE = 'cannot_coerce'
+	;
+  END $$ LANGUAGE plpgsql
+;
+
+CREATE TRIGGER check_jsonability AFTER INSERT
+  ON
+  	jsonorg.jsonb_255
+  FOR EACH ROW EXECUTE PROCEDURE
+  	check_jsonability()
+;
+
+COMMENT ON TABLE jsonorg.jsonb_255 IS
 	'Binary encoding of valid json document from table checker_255'
 ;
 
-create index idx_jsonb_255_gin
-  on
-  	jsonorg.jsonb_255 using gin (doc)
+CREATE INDEX idx_jsonb_255_gin
+  ON
+  	jsonorg.jsonb_255 USING gin(doc)
 ;
 
---  for indexing the @> operator
-create index idx_jsonb_255_pgin
-  on
-  	jsonorg.jsonb_255 using gin (doc jsonb_path_ops)
+--  indexing the @> operator
+CREATE INDEX idx_jsonb_255_pgin
+  ON
+  	jsonorg.jsonb_255 USING gin (doc jsonb_path_ops)
 ;
 
-commit;
+REVOKE UPDATE ON ALL TABLES IN SCHEMA jsonorg FROM PUBLIC;
+
+COMMIT;
