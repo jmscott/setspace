9154873bcc859e22d9c5707a0422383fc4990ea4
John the Scott <jmscott@setspace.com>
2015-11-14 23:40:00 -0600

    zapped pdfbox

--- schema/pdfbox/local.mk
+++
@@ -1,9 +0,0 @@
-#
-#  Synopsis:
-#	Example development local.mk file used by Makefile.
-#
-PDFBOX_PREFIX=/home/jmscott/opt/pdfbox
-PDFBOX_USER=jmscott
-PDFBOX_GROUP=jmscott
-CFLAGS=-Wall -Wextra
-PGROOT=/usr/local/pgsql
