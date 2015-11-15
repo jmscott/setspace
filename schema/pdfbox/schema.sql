3398eaae5b3f472c29863219a44fd8a1db20ca5e
John the Scott <jmscott@setspace.com>
2015-11-14 18:55:21 -0600

    extract_utf8(blob) references pddocument(blob)

--- schema/pdfbox/schema.sql
+++ schema/pdfbox/schema.sql
@@ -45,7 +45,7 @@ DROP TABLE IF EXISTS pdfbox.extract_utf8;
 CREATE TABLE pdfbox.extract_utf8
 (
 	blob		udig
-				references setspace.service(blob)
+				references pdfbox.pddocument(blob)
 				on delete cascade
 				primary key,
 
@@ -58,10 +58,12 @@ CREATE TABLE pdfbox.extract_utf8
 	utf8_blob	udig,
 	stderr_blob	udig,
 
+	--  no quines
 	constraint utf8_not_blob check (
 		blob != utf8_blob
 	),
 
+	--  no quines
 	constraint stderr_not_blob check (
 		blob != stderr_blob
 	)
