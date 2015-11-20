1cb4992386e2ed81017141540491478d6cda2ff0
John the Scott <jmscott@setspace.com>
2015-11-20 07:32:14 -0600

    first cut at bulk json to table merger

--- schema/pdfbox2/merge-json.sql
+++ schema/pdfbox2/merge-json.sql
@@ -1,28 +1,45 @@
 /*
  *  Synopsis:
  *	merge jsonorg pdfbox2.setspace.com objects into pdfbox2.* tables
+ *  Usage:
+ *	psql -f merge-json.sql --variable=since="'-1 day'"
  *  Note:
  *	Unfortunatly, we have stale pdfbox.setspace.com json objects laying
  *	around.
  */
 
+\echo merging all blobs since :since
+
+\timing on
+\set ON_ERROR_STOP on
+
+create temp table merge_utf8 as
 /*
  *  Merge jsonorg.jsonb_255{pdfbox.setspace.com->extract_utf8} into the table
  *  pdfbox2.extract_utf8.
  */
 with find_utf8 as (
   select
+  	blob as jblob,
   	doc->'pdfbox.setspace.com'->'extract_utf8' as doc
   from
   	jsonb_255
+	  natural join setspace.service s
   where
   	doc @> '{
 	    "pdfbox.setspace.com": {
 	      "extract_utf8": {}
 	    }
 	}'
-), x_utf8 as (
+	and
+	s.discover_time > now() + :since
+),
+  /*
+   *  Extract UTF8 from json.
+   */
+  x_utf8 as (
 	select
+		jblob,
 		blob,
 		case
 		  when
@@ -56,11 +73,47 @@ with find_utf8 as (
 		)
 )
 select
-	blob
+	*,
+	'Unknown'::text as "syncability"
   from
   	x_utf8
-  order by
-  	random()
-  limit
+;
+
+\echo analyze merge_utf8
+analyze merge_utf8;
+
+\echo indexing merge_utf8(blob)
+create index merge_utf8_idx_u on merge_utf8(blob);
+
+\echo indexing merge_utf8(blob)
+create unique index merge_utf8_idx_j on merge_utf8(jblob);
+
+\echo tagging blobs already merged
+
+\echo tagging duplicate blobs in different json runs not yet merged
+update merge_utf8 m1
+  set
+  	syncability = 'Duplicate'
+  where
+  	exists (
+	  select
+	  	m2.blob
+	  from
+	  	merge_utf8 m2
+	  where
+	  	m2.blob = m1.blob
+		and
+		m2.jblob != m1.jblob
+	)
+;
+
+select
+	syncability,
+	count(*) as "UTF8 Blob Count"
+  from
+  	merge_utf8
+  group by
   	1
+  order by
+  	"UTF8 Blob Count" desc
 ;
