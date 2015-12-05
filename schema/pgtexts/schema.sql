c0e80f6f68580471c898fd76e205d3075f1de1a2
John the Scott <jmscott@setspace.com>
2015-12-04 22:50:50 -0600

    created pg text search schema named "pgtexts"

---
+++ schema/pgtexts/schema.sql
@@ -0,0 +1,48 @@
+/*
+ *  Synopsis:
+ *	Database schema for PostgreSQL text Search
+ */
+\set ON_ERROR_STOP
+
+DROP SCHEMA IF EXISTS pgtexts CASCADE;
+CREATE SCHEMA pgtexts;
+COMMENT ON SCHEMA pgtexts IS
+  'Text and metadata extracted by pdfbox.apache.org, version 2'
+;
+
+/*
+ *  Note:
+ *	Is the ts_config bound to the tsvector data item?
+ *	In other words, is a ts_conf field needed in table?
+ */
+DROP TABLE IF EXISTS pgtexts.tsv_utf8;
+CREATE TABLE pgtexts.tsv_utf8
+(
+	ts_conf		regconfig,
+	blob		udig
+				REFERENCES setspace.is_utf8wf(blob)
+				ON DELETE CASCADE,
+	doc		tsvector
+				not null,
+
+	PRIMARY KEY	(ts_conf, blob)
+);
+
+CREATE UNIQUE INDEX tsv_utf8_ts_blob ON pgtexts.tsv_utf8(blob, ts_conf);
+
+COMMENT ON TABLE pgtexts.tsv_utf8 IS
+  'Text Vector of Extracted UTF8 Text'
+;
+
+DROP TABLE IF EXISTS pgtexts.tsv_utf8_pending;
+CREATE TABLE pgtexts.merge_tsv_utf8_pending
+(
+	blob		udig
+				PRIMARY KEY,
+	insert_time	timestamptz
+				DEFAULT now()
+				NOT NULL
+);
+COMMENT ON TABLE pgtexts.merge_tsv_utf8_pending IS
+  'Actively running processes for merge_tsv_utf8'
+;
