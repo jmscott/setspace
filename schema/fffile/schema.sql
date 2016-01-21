9357edcd8eb800f6da3901b2137e408a8ee17f4b
John the Scott <jmscott@setspace.com>
2016-01-20 21:16:13 -0600

    added upsert and renamed file-json to merge-file

--- schema/fffile/schema.sql
+++ schema/fffile/schema.sql
@@ -27,6 +27,7 @@ CREATE TABLE fffile.file
 				PRIMARY KEY,
 
 	--  null indicates file produces non-utf8 output and exit status == 0
+	--  trailing new-lines have been trimmed
 	file_type	text
 );
 COMMENT ON TABLE fffile.file IS
