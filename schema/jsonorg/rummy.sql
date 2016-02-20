fab72cd531d29581343ef797eff7b55bfc5e8edc
John the Scott <jmscott@setspace.com>
2016-02-19 21:09:24 -0600

    created rummy.sql

---
+++ schema/jsonorg/rummy.sql
@@ -0,0 +1,52 @@
+/*
+ *  Synopsis:
+ *	Find blobs with known unknowns in schema "jsonorg".
+ */
+WITH json_candidate AS (
+  SELECT
+  	bjb.blob
+    FROM
+	setspace.has_byte_json_bracket bjb
+	  INNER JOIN setspace.service s ON (s.blob = bjb.blob)
+    WHERE
+  	bjb.has_bracket is true
+	AND
+	s.discover_time >= now() + :since
+)
+
+--  json candidates not in table checker_255
+
+SELECT
+	jc.blob
+  FROM
+	json_candidate jc
+  WHERE
+	NOT EXISTS (
+	  SELECT
+	  	ch.blob
+	    FROM
+	  	jsonorg.checker_255 ch
+	    WHERE
+	    	ch.blob = jc.blob
+	)
+
+  --  verified json blobs not in table json_255
+
+  UNION (
+    SELECT
+  	ch.blob
+      FROM
+    	jsonorg.checker_255 ch
+      WHERE
+      	ch.is_json is true
+	AND
+    	NOT EXISTS (
+	  SELECT
+	  	jj.blob
+	    FROM
+	    	jsonorg.jsonb_255 jj
+	    WHERE
+	    	jj.blob = ch.blob
+	)
+)
+;
