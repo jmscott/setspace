3dfc3461f291309e47a80c3ef66c67536bcdfe3d
John the Scott <jmscott@setspace.com>
2016-02-20 14:48:54 -0600

    find unecrypted, failed text utf extracts

---
+++ schema/pdfbox2/extract_utf8-fail.sql
@@ -0,0 +1,13 @@
+select
+	ex.blob
+  from
+  	pdfbox2.extract_utf8 ex
+	  inner join setspace.service s on (s.blob = ex.blob)
+	  inner join pdfbox2.pddocument pd on (pd.blob = ex.blob)
+  where
+  	s.discover_time >= now() + :since
+	and
+	pd.is_encrypted = false
+	and
+	ex.exit_status != 0
+;
