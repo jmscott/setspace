03a5f4f261a1ff586b3ff9cf25af737e8776e492
John the Scott <jmscott@setspace.com>
2016-02-22 08:13:07 -0600

    added :since variables, changed qdr/xdr detail to "exists" clause

--- schema/drblob/rummy.sql
+++ schema/drblob/rummy.sql
@@ -10,8 +10,9 @@ select
 	bl.blob
   from
   	drblob.is_brr_log bl
-	  natural left outer join drblob.brr_log_total tot
-	  natural left outer join drblob.brr_log_time tim
+	  inner join setspace.service s on (s.blob = bl.blob)
+	  left outer join drblob.brr_log_total tot on (tot.blob = bl.blob)
+	  left outer join drblob.brr_log_time tim on (tim.blob = bl.blob)
   where
   	bl.is_brr is true
 	and
@@ -20,14 +21,17 @@ select
 		or
 		tim.blob is null
 	)
+	and
+	s.discover_time >= now() + :since
 UNION
 select
-	fl.blob as "Rummy"
+	fl.blob
   from
   	drblob.is_fdr_log fl
-	  natural left outer join drblob.fdr_log_total tot
-	  natural left outer join drblob.fdr_log_time tim
-	  natural left outer join drblob.fdr_log_sequence seq
+	  inner join setspace.service s on (s.blob = fl.blob)
+	  left outer join drblob.fdr_log_total tot on (tot.blob = fl.blob)
+	  left outer join drblob.fdr_log_time tim on (tim.blob = fl.blob)
+	  left outer join drblob.fdr_log_sequence seq on (seq.blob = fl.blob)
   where
   	fl.is_fdr is true
 	and
@@ -38,15 +42,19 @@ select
 		or
 		seq.blob is null
 	)
+	and
+	s.discover_time >= now() + :since
 UNION
 select
 	ql.blob
   from
   	drblob.is_qdr_log ql
-	  natural left outer join drblob.qdr_log_total tot
-	  natural left outer join drblob.qdr_log_time tim
-	  natural left outer join drblob.qdr_log_flow_sequence seq
-	  natural left outer join drblob.qdr_log_query q
+	  inner join setspace.service s on (s.blob = ql.blob)
+	  left outer join drblob.qdr_log_total tot on (tot.blob = ql.blob)
+	  left outer join drblob.qdr_log_time tim on (tim.blob = ql.blob)
+	  left outer join drblob.qdr_log_flow_sequence seq on (
+	  	seq.blob = ql.blob
+	  )
   where
   	ql.is_qdr is true
 	and
@@ -57,17 +65,31 @@ select
 		or
 		seq.blob is null
 		or
-		q.blob is null
+
+		--  at least one query detail record must exist
+
+		not exists (
+		  select
+		  	q.blob
+		    from
+		    	drblob.qdr_log_query q
+		    where
+		    	q.blob = ql.blob
+		)
 	)
+	and
+	s.discover_time >= now() + :since
 UNION
 select
 	xl.blob
   from
   	drblob.is_xdr_log xl
-	  natural left outer join drblob.xdr_log_total tot
-	  natural left outer join drblob.xdr_log_time tim
-	  natural left outer join drblob.xdr_log_flow_sequence seq
-	  natural left outer join drblob.xdr_log_query q
+	  inner join setspace.service s on (s.blob = xl.blob)
+	  left outer join drblob.xdr_log_total tot on (tot.blob = xl.blob)
+	  left outer join drblob.xdr_log_time tim on (tim.blob = xl.blob)
+	  left outer join drblob.xdr_log_flow_sequence seq on (
+		seq.blob = xl.blob
+	  )
   where
   	xl.is_xdr is true
 	and
@@ -78,6 +100,18 @@ select
 		or
 		seq.blob is null
 		or
-		q.blob is null
+
+		--  at least one exec detail record must exist
+
+		not exists (
+		  select
+		  	x.blob
+		    from
+		    	drblob.xdr_log_query x
+		    where
+		    	x.blob = xl.blob
+		)
 	)
+	and
+	s.discover_time >= now() + :since
 ;
