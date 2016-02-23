edd193d229b733577f47d61a8794ffc775388fb7
John the Scott <jmscott@setspace.com>
2016-02-22 21:11:23 -0600

    comment

--- schema/expat2/schema.sql
+++ schema/expat2/schema.sql
@@ -18,6 +18,10 @@ SET search_path TO expat2,setspace,public;
 /*
  *  Is the blob well formed xml according to xmlwf program, version 2,
  *  in the expat library.
+ *
+ *  Note:
+ *	Table 'is_xmlwf' ought to just be a table named 'xmlwf' with the
+ *	exit_status.
  */
 DROP TABLE IF EXISTS expat2.is_xmlwf;
 CREATE TABLE expat2.is_xmlwf
