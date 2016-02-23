509d508f6ab0a2ae5f1a0e1926bebab80457b473
John the Scott <jmscott@setspace.com>
2016-02-23 06:38:03 -0600

    upercased sql keywords

--- schema/drblob/create-temp-merge_qdr_log.sql
+++ schema/drblob/create-temp-merge_qdr_log.sql
@@ -2,61 +2,59 @@
  *  Synopsis:
  *	Create a temporary table suitable for query detail records
  *  Usage:
+ *	#  in psql
  *	\i lib/create-temp-merge_qdr_log.sql
  *  See:
- *	lib/merge-qdr_log_total.sql
- *	lib/merge-qdr_log_time.sql
- *  Blame:
- *  	jmscott@setspace.com
- *  	setspace@gmail.com
+ *	schema/drblob/lib/merge-qdr_log_total.sql
+ *	schema/drblob/lib/merge-qdr_log_time.sql
+ *	schema/drblob/lib/merge-qdr_log_flow_sequence.sql
+ *	schema/drblob/lib/merge-qdr_log_query.sql
  *  Note:
  *	Constraint for wall_duration >= query_duration?
  *
- *	Use the blobio domains for timestamp, name, etc.
+ *	Use the blobio domains for timestamp, name, etc?
  */
-create temporary table merge_qdr_log
+CREATE TEMPORARY TABLE merge_qdr_log
 (
 	start_time		timestamptz
-					not null,
+					NOT NULL,
 
-	flow_sequence		bigint check (
+	flow_sequence		bigint CHECK (
 					flow_sequence > 0
-				)
-				not null,
+				) NOT NULL,
 
 	query_name		text check (
 					query_name ~
 					       '^[[:alpha:]][[:alnum:]_]{0,64}$'
-				)
-				not null,
+				) NOT NULL,
 
-	termination_class	text check (
+	termination_class	text CHECK (
 					termination_class in (
 						'OK',
 						'ERR'
 					)
-				) not null,
+				) NOT NULL,
 
 	blob			udig
-					not null,
+					NOT NULL,
 
-	sqlstate		text check (
+	--  sqlstate ought to be a domain
+
+	sqlstate		text CHECK (
 					sqlstate ~ '^[0-9A-Z]{5}$'
-				) not null,
+				) NOT NULL,
 
-	rows_affected		bigint check (
+	rows_affected		bigint CHECK (
 					rows_affected >= 0
-				) not null,
+				) NOT NULL,
 
-	wall_duration		interval check (
+	wall_duration		interval CHECK (
 					wall_duration >= '0'::interval
-				)
-				not null,
+				) NOT NULL,
 
-	query_duration		interval check (
+	query_duration		interval CHECK (
 					query_duration >= '0'::interval
-				)
-				not null
+				) NOT NULL
 );
 \copy merge_qdr_log from pstdin
-analyze merge_qdr_log;
+ANALYZE merge_qdr_log;
