1e2bed94fe5b416198b6f16f94e7420b6d07a535
John the Scott <jmscott@setspace.com>
2016-05-14 06:53:28 -0500

    qualified on schema pgtexts.

--- schema/pgtexts/schema.sql
+++ schema/pgtexts/schema.sql
@@ -48,7 +48,7 @@ CREATE UNIQUE INDEX tsv_utf8_ts_blob ON pgtexts.tsv_utf8(blob, ts_conf);
 COMMENT ON TABLE pgtexts.tsv_utf8 IS
   'Text Vector of Extracted UTF8 Text'
 ;
-CREATE INDEX tsv_utf8_doc ON tsv_utf8 USING gin(doc);
+CREATE INDEX tsv_utf8_doc ON pgtexts.tsv_utf8 USING gin(doc);
 
 DROP TABLE IF EXISTS pgtexts.merge_tsv_utf8_pending;
 CREATE TABLE pgtexts.merge_tsv_utf8_pending
@@ -115,7 +115,7 @@ CREATE UNIQUE INDEX tsv_strip_utf8_ts_blob ON
 COMMENT ON TABLE pgtexts.tsv_strip_utf8 IS
   'Stripped Text Vector of Extracted UTF8 Text'
 ;
-CREATE INDEX tsv_strip_utf8_doc ON tsv_strip_utf8 USING gin(doc);
+CREATE INDEX tsv_strip_utf8_doc ON pgtexts.tsv_strip_utf8 USING gin(doc);
 
 DROP TABLE IF EXISTS pgtexts.merge_tsv_strip_utf8_pending;
 CREATE TABLE pgtexts.merge_tsv_strip_utf8_pending
