4d95594d6ec9347887f11d19276b82c4b70b8dff
John the Scott <jmscott@setspace.com>
2016-05-23 08:33:28 -0500

    added logging to rest-pdfbox2

--- schema/pdfbox2/rest-pdfbox2.go
+++ schema/pdfbox2/rest-pdfbox2.go
@@ -3,15 +3,72 @@ package main
 import (
 	"fmt"
 	"html"
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"runtime"
+	"time"
 )
 
+var listen string = ":8080";
+
+var (
+	stderr = os.NewFile(uintptr(syscall.Stderr), "/dev/stderr")
+	stdout = os.NewFile(uintptr(syscall.Stdout), "/dev/stdout")
+)
+
+func usage() {
+	fmt.Fprintf(stderr, "usage: rest-pdfbox2\n")
+}
+
+func ERROR(format string, args ...interface{}) {
+
+	fmt.Fprintf(
+		stderr,
+		"%s: rest-pdfbox2: ERROR: %s\n",
+		time.Now().Format("2006/01/02 15:04:05"),
+		fmt.Sprintf(format, args...),
+	)
+}
+
+func log(format string, args ...interface{}) {
+
+	fmt.Fprintf(stdout, "%s: %s\n",
+		time.Now().Format("2006/01/02 15:04:05"),
+		fmt.Sprintf(format, args...),
+	)
+}
+
+func leave(status int) {
+	log("good bye, cruel world")
+	os.Exit(status);
+}
+
 func main() {
+
+	log("hello, world");
+
+	log("process id: %d", os.Getpid())
+	log("go version: %s", runtime.Version())
+	log("listen service: %s", listen)
+
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGTERM)
+		s := <-c
+		log("caught signal: %s", s)
+		leave(0)
+	}()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := http.ListenAndServe(listen, nil)
+	if err != nil {
+		ERROR("%s", err)
+		leave(64)
+	}
+	leave(0)
 }
