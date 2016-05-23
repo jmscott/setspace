f1dcc21ffb6adfc11c004a1c9ed1bf393be8973d
John the Scott <jmscott@setspace.com>
2016-05-23 10:34:57 -0500

    checkpoint

--- schema/pdfbox2/rest-pdfbox2.go
+++ schema/pdfbox2/rest-pdfbox2.go
@@ -1,13 +1,15 @@
+//rest service for pdfbox2 schema.  do /pdfbox2/help for details
 package main
 
 import (
 	"fmt"
 	"html"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
 	"runtime"
+	"syscall"
 	"time"
 )
 
@@ -45,30 +47,56 @@ func leave(status int) {
 	os.Exit(status);
 }
 
+func die(format string, args ...interface{}) {
+
+	ERROR(format, args)
+	leave(2)
+}
+
+func slurp_file(path string) string {
+	
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		die("%s", err)
+	}
+	return string(buf)
+}
+
 func main() {
 
 	log("hello, world");
+
+	if len(os.Args) != 1 {
+		die(
+			"wrong number of arguments: got %d, expected 1",
+			len(os.Args),
+		)
+	}
 
 	log("process id: %d", os.Getpid())
 	log("go version: %s", runtime.Version())
 	log("listen service: %s", listen)
 
+	//  catch signals
+
 	go func() {
 		c := make(chan os.Signal)
 		signal.Notify(c, syscall.SIGTERM)
+		signal.Notify(c, syscall.SIGQUIT)
+		signal.Notify(c, syscall.SIGINT)
 		s := <-c
 		log("caught signal: %s", s)
 		leave(0)
 	}()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+
+		fmt.Fprintf(w, "Not Found: %q", html.EscapeString(r.URL.Path))
 	})
 
 	err := http.ListenAndServe(listen, nil)
 	if err != nil {
-		ERROR("%s", err)
-		leave(64)
+		die("%s", err)
 	}
 	leave(0)
 }
