5d2dd80fc06ce8de65a6bde2d37d18a7f20e4359
John the Scott <jmscott@setspace.com>
2016-07-25 14:32:35 -0500

    zapped pgtext query, added new page_{text,tsv}_utf8 queries

--- schema/pdfbox2/rummy.sql
+++ schema/pdfbox2/rummy.sql
@@ -5,7 +5,10 @@
  *	psql -f rummy.sql --set since="'-1 week'"
  */
 
-WITH pdf_candidate AS (
+--  blob with pdf prefix %PDF-
+--  Note:  need to check suffix of pdf blob
+
+WITH pdf_prefix_candidate AS (
 
   --  find all recent pdf candidates which are not in any pending jobs
 
@@ -14,12 +17,14 @@ WITH pdf_candidate AS (
     FROM
     	setspace.byte_prefix_32 pre
 	  INNER JOIN setspace.service s ON (s.blob = pre.blob)
+	  INNER JOIN setspace.byte_count bc ON (bc.blob = pre.blob)
 	  LEFT OUTER JOIN pdfbox2.pddocument_pending pdp ON
 	  	(pdp.blob = pre.blob)
 	  LEFT OUTER JOIN pdfbox2.extract_pages_utf8_pending eup ON
 	  	(eup.blob = pre.blob)
     WHERE
 	--  Note: need to check all core tables in setspace, not just prefix!!
+
   	substring(pre.prefix, 1, 4) = '\x25504446'
 	AND
 	s.discover_time between (now() + :since) and (now() + '-1 minute')
@@ -34,54 +39,67 @@ WITH pdf_candidate AS (
 SELECT
 	pdf.blob
   FROM
-  	pdf_candidate pdf
+  	pdf_prefix_candidate pdf
 	  LEFT OUTER JOIN pdfbox2.pddocument pd ON (pd.blob = pdf.blob)
   WHERE
   	pd.blob is NULL
 
---  parsable pdfs in pddocument and not in extract_pages_utf8
+/*
+ *  Parsable pdfs in pddocument and not in
+ *  extract_pages_utf8 and extract_pages_utf8_pending,
+ *  merge_pages_text_utf8  merge_pages_text_utf8_pending,
+ *  merge_pages_tsv_utf8  merge_pages_tsv_utf8_pending,
+ */  
 
 UNION (
   SELECT
   	pd.blob
     FROM
-    	pdf_candidate pdf
+    	pdf_prefix_candidate pdf
     	  INNER JOIN pdfbox2.pddocument pd ON (pd.blob = pdf.blob)
 	  LEFT OUTER JOIN pdfbox2.extract_pages_utf8 ex ON (
 	  	ex.blob = pd.blob
 	  )
+	  LEFT OUTER JOIN pdfbox2.extract_pages_utf8_pending exp ON (
+	  	exp.blob = pd.blob
+	  )
+	  LEFT OUTER JOIN pdfbox2.merge_pages_text_utf8 txt ON (
+	  	txt.blob = pd.blob
+	  )
+	  LEFT OUTER JOIN pdfbox2.merge_pages_text_utf8_pending txtp ON (
+	  	txtp.blob = pd.blob
+	  )
+	  LEFT OUTER JOIN pdfbox2.merge_pages_tsv_utf8 tsv ON (
+	  	tsv.blob = pd.blob
+	  )
+	  LEFT OUTER JOIN pdfbox2.merge_pages_tsv_utf8_pending tsvp ON (
+	  	tsvp.blob = pd.blob
+	  )
     WHERE
     	pd.exit_status = 0
-	AND
-	ex.blob IS NULL
-) UNION
-
---  extracted utf8 pages with no page text search vector or page text doc
-
-(
-  SELECT
-  	ex.pdf_blob
-    FROM
-    	pdf_candidate pdf
-	  JOIN pdfbox2.extract_page_utf8 ex ON (ex.pdf_blob = pdf.blob)
-    WHERE
-	 --  extracted page utf8 blob not in text search vector or blob table
-	NOT EXISTS (
-	  SELECT
-	  	tsv.blob
-	    FROM
-	    	pgtexts.tsv_utf8 tsv
-	    WHERE
-	    	tsv.blob = ex.page_blob
-	)
-	OR
-	NOT EXISTS (
-	  SELECT
-	  	txt.blob
-	    FROM
-	    	pgtexts.text_utf8 txt
-	    WHERE
-	    	txt.blob = ex.page_blob
+	AND (
+		--  not in extract_pages_utf8 and 
+		--  extract_pages_utf8_pending
+		(
+			ex.blob IS NULL
+			AND
+			exp.blob IS NULL
+		)
+		OR
+		--  not in merge_pages_text_utf8 and 
+		--  pdfbox2.merge_pages_text_utf8_pending
+		(
+			txt.blob IS NULL
+			AND
+			txtp.blob IS NULL
+		)
+		OR
+		--  not in merge_pages_tsv_utf8 and 
+		--  pdfbox2.merge_pages_tsv_utf8_pending
+		(
+			tsv.blob IS NULL
+			AND
+			tsvp.blob IS NULL
+		)
 	)
 )
-;
