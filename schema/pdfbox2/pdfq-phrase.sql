86b5631dff9735bec50aaeaf58312843237c7fc2
John the Scott <jmscott@setspace.com>
2016-08-04 09:46:50 -0500

    repatched miscommit bug

--- schema/pdfbox2/pdfq-phrase.sql
+++ schema/pdfbox2/pdfq-phrase.sql
@@ -34,11 +34,9 @@ with pdf_page_match as (
   where
   	tsv.tsv @@ q
 	and
-	tsv.ts_conf = :ts_conf
-	and
-	tsv.tsv <=> q < 
+	tsv.ts_conf = :ts_conf::text
   group by
-  	tsv.pdf_blob
+  	1
   order by
   	page_rank_sum desc,
 	match_page_count desc
@@ -70,7 +68,7 @@ with pdf_page_match as (
 	    where
   		tsv.tsv @@ q
 		and
-		tsv.ts_conf = :ts_conf
+		tsv.ts_conf = :ts_conf::text
 		and
 		tsv.pdf_blob = pd.blob
 	    order by
