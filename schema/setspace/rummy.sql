f44aea79cee5233946baf0e735cfbb143d8b45d0
John the Scott <jmscott@setspace.com>
2019-03-23 17:02:19 -0500

    zapped json,xml and newline

--- schema/setspace/rummy.sql
+++ schema/setspace/rummy.sql
@@ -7,47 +7,29 @@
  *  	jmscott@setspace.com
  *  	setspace@gmail.com
  */
-with recent_service as (
-  select
-  	s.blob
-    from
-    	setspace.service s
-    where
-	s.discover_time between (now() + :since) and (now() + '-1 minute')
-)
- select
-	rs.blob
-  from
-  	recent_service rs
-	  left outer join setspace.byte_count bc on (bc.blob = rs.blob)
-	  left outer join setspace.byte_bitmap bm on (bm.blob = rs.blob)
-	  left outer join setspace.byte_prefix_32 bp32 on (bp32.blob = rs.blob)
-	  left outer join setspace.byte_suffix_32 bs32 on (bs32.blob = rs.blob)
-	  left outer join setspace.is_utf8wf u8 on (u8.blob = rs.blob)
+set search_path TO setspace,public;
+
+SELECT
+	DISTINCT s.blob
+  FROM
+  	setspace.service s
+	  LEFT OUTER JOIN byte_count bc ON (bc.blob = s.blob)
+	  LEFT OUTER JOIN byte_bitmap bm ON (bm.blob = s.blob)
+	  LEFT OUTER JOIN byte_prefix_32 bp32 ON (bp32.blob = s.blob)
+	  LEFT OUTER JOIN byte_suffix_32 bs32 ON (bs32.blob = s.blob)
+	  LEFT OUTER JOIN is_utf8wf u8 ON (u8.blob = s.blob)
   where
-  	bc.blob is null
-	or
-	bm.blob is null
-	or
-	u8.blob is null
-	or
-	bp32.blob is null
-	or
-	bs32.blob is null
- union (
-  select
-	u8.blob
-    from
-  	setspace.is_utf8wf u8
-	  inner join recent_service rs on (rs.blob = u8.blob)
-	  left outer join setspace.is_udigish ud on (ud.blob = rs.blob)
-	  left outer join setspace.new_line_count nc on (nc.blob = rs.blob)
-    where
-  	u8.is_utf8 = true
-	and
+  	s.discover_time >= now() + :since
+	AND
 	(
-		ud.blob is null
-		or
-		nc.blob is null
+		bc.blob IS NULL
+		OR
+		bm.blob IS NULL
+		OR
+		u8.blob IS NULL
+		OR
+		bp32.blob IS NULL
+		OR
+		bs32.blob IS NULL
 	)
-)
+;
