3487888378ad52956dd124f129c4abc563fec5a8
John the Scott <jmscott@setspace.com>
2019-03-29 18:13:22 -0500

    broken checkpoint

--- schema/jsonorg/rummy.sql
+++ schema/jsonorg/rummy.sql
@@ -1,52 +1,35 @@
-/*
- *  Synopsis:
- *	Find blobs with known unknowns in schema "jsonorg".
- */
-WITH json_candidate AS (
+\timing
+
+WITH utf8wf AS (
   SELECT
-  	bjb.blob
+  	blob
     FROM
-	setspace.has_byte_json_bracket bjb
-	  INNER JOIN setspace.service s ON (s.blob = bjb.blob)
+    	setspace.is_utf8wf
     WHERE
-  	bjb.has_bracket IS TRUE
-	AND
-	s.discover_time BETWEEN (now() + :since) AND (now() + '-1 minute')
+	is_utf8 IS TRUE
 )
-
---  json candidates not in table checker_255
-
 SELECT
-	jc.blob
+	u8.blob
   FROM
-	json_candidate jc
+  	utf8wf u8
+	  JOIN setspace.byte_prefix_32 p32 ON (p32.blob = u8.blob)
+	  JOIN setspace.byte_suffix_32 s32 ON (s32.blob = u8.blob)
+	  LEFT OUTER JOIN jsonorg.checker_255 js ON (js.blob = u8.blob)
   WHERE
-	NOT EXISTS (
-	  SELECT
-	  	ch.blob
-	    FROM
-	  	jsonorg.checker_255 ch
-	    WHERE
-	    	ch.blob = jc.blob
+	(
+		(	--  utf8 framed with {}
+
+			convert_from(p32.prefix, 'UTF-8') ~ '^\s*'
+			AND
+			convert_from(s32.suffix, 'UTF-8') ~ '}\s*$'
+		)
+		OR
+		(	--  utf8 framed with []
+			convert_from(p32.prefix, 'UTF-8') ~ '^\s*[[]'
+			AND
+			convert_from(s32.suffix, 'UTF-8') ~ ']\s*$'
+		)
 	)
-
-  --  verified json blobs not in table json_255
-
-  UNION (
-    SELECT
-  	ch.blob
-      FROM
-    	jsonorg.checker_255 ch
-      WHERE
-      	ch.is_json is true
 	AND
-    	NOT EXISTS (
-	  SELECT
-	  	jj.blob
-	    FROM
-	    	jsonorg.jsonb_255 jj
-	    WHERE
-	    	jj.blob = ch.blob
-	)
-)
+	js.blob IS NULL
 ;
