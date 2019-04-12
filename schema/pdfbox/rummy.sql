326410875cff5b822be74af5a4a4f9e7a8bbea07
John the Scott <jmscott@setspace.com>
2019-04-12 14:10:32 -0500

    added table pddocument_information

--- schema/pdfbox/rummy.sql
+++ schema/pdfbox/rummy.sql
@@ -1,8 +1,6 @@
 /*
  *  Synopsis:
  *	Find unresolved pddocument and extract_pages_ut8 blobs
- *  Command Line Variables:
- *	since	text
  *  Usage:
  *	psql -f rummy.sql --set since="'-1 week'"
  */
@@ -11,11 +9,20 @@ SELECT
   	pre.blob
     FROM
     	setspace.byte_prefix_32 pre
+	  --  discovered after certain time
 	  INNER JOIN setspace.service s ON (s.blob = pre.blob)
+
+	  --  pddocument table
 	  LEFT OUTER JOIN pdfbox.pddocument pd ON
 	  	(pd.blob = pre.blob)
 	  LEFT OUTER JOIN pdfbox.fault_pddocument fpd ON
 	  	(fpd.blob = pre.blob)
+
+	  --  pddocument_information table
+	  LEFT OUTER JOIN pdfbox.pddocument_information pdi ON
+	  	(pdi.blob = pre.blob)
+	  LEFT OUTER JOIN pdfbox.fault_pddocument_information fpdi ON
+	  	(fpdi.blob = pre.blob)
     WHERE
 	--  Note: need to check all core tables in setspace, not just prefix!!
 
@@ -23,7 +30,11 @@ SELECT
 	AND
 	s.discover_time >= now() + :since::interval
 	AND
-	pd.blob IS NULL
-	AND
-	fpd.blob IS NULL
+	(
+		--  not in either [fault_]pddocument
+		(pd.blob IS NULL AND fpd.blob IS NULL)
+		OR
+		--  not in either [fault_]pddocument_information
+		(pdi.blob IS NULL AND fpdi.blob IS NULL)
+	)
 ;
