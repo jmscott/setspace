f0d0fc77f1b5e1de8e5f04c482f9a28b261bd036
John the Scott <jmscott@setspace.com>
2019-07-16 19:42:58 -0500

    replaced table is_xmlwf with xmlwf

--- schema/expat2/schema.sql
+++ schema/expat2/schema.sql
@@ -12,25 +12,23 @@ DROP SCHEMA IF EXISTS expat2 CASCADE;
 
 CREATE SCHEMA expat2;
 
-SET search_path TO expat2,setcore,public;
+SET search_path TO expat2,public;
 
 /*
- *  Is the blob well formed xml according to xmlwf program, version 2,
- *  in the expat library.
- *
- *  Note:
- *	Table 'is_xmlwf' ought to just be a table named 'xmlwf' with the
- *	exit_status.
+ *  The output of the xmlwf program.
+ *  Well formed xml is implied by zero length(stdout).
+ *  Manual says exit code is always 0.
  */
-DROP TABLE IF EXISTS expat2.is_xmlwf;
-CREATE TABLE expat2.is_xmlwf
+DROP TABLE IF EXISTS xmlwf_utf8;
+CREATE TABLE xmlwf_utf8
 (
 	blob	udig
 			REFERENCES setcore.service
 			ON DELETE CASCADE
 			PRIMARY KEY,
-	is_xml	bool
-			NOT NULL
+	stdout	text	CHECK (
+				length(stdout) < 256
+			) NOT NULL
 );
 
 COMMIT;
