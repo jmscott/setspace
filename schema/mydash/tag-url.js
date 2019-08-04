50cb8983732386721ef3c829a7e0bbf0e97f458c
jmscott <jmscott@setspace.com>
2019-08-04 16:49:46 -0500

    added https to location.href

--- schema/mydash/tag-url.js
+++ schema/mydash/tag-url.js
@@ -1,1 +1,1 @@
-javascript:location.href='{{HTTP_HOST}}/cgi-bin/tag-url?out=save&url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title);
+javascript:location.href='https://{{HTTP_HOST}}/cgi-bin/tag-url?out=save&url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title);
