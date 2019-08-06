791c01d3047a606dbfe000ebeb75452d6ce2e15f
jmscott <jmscott@setspace.com>
2019-08-06 18:44:35 -0500

    added html widget pdfbox.d/dl.pl

---
+++ schema/mydash/pdfbox.d/dl.pl
@@ -0,0 +1,36 @@
+#
+#  Synopsis:
+#	Write html <dl> of pdf documents that match keyword search.
+#  Usage:
+#	/cgi-bin/pdfbox?out=dl&q=neural+cryptography
+#
+use Time::HiRes qw(gettimeofday);
+
+require 'dbi-pg.pl';
+require 'httpd2.d/common.pl';
+require 'pdfbox.d/common.pl';
+
+our %QUERY_ARG;
+
+print <<END;
+<dl $QUERY_ARG{id_att}$QUERY_ARG{class_att}>
+END
+
+my $qh = recent_select();
+while (my $r = $qh->fetchrow_hashref()) {
+	my $blob = encode_html_entities($r->{blob});
+	my $title = encode_html_entities($r->{title});
+	$title = 'No Title' unless $title =~ m/[[:graph:]]/;
+	my $number_of_pages = $r->{number_of_pages};
+	my $discover_time = $r->{discover_time};
+	my $plural_nop = 's';
+	$plural_nop = '' if $number_of_pages == 1;
+	print <<END;
+ <dt>$title</dt>
+ <dd>$number_of_pages page$plural_nop, discovered $discover_time</dd>
+END
+}
+
+print <<END;
+</dl>
+END
