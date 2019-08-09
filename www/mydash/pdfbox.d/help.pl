13d8a26d1339b92d3b9f89a9b108376af52a81f3
jmscott <jmscott@setspace.com>
2019-08-09 11:20:53 -0500

    copied schema/mydash to www/mydash

---
+++ www/mydash/pdfbox.d/help.pl
@@ -0,0 +1,214 @@
+#
+#  Synopsis:
+#	Write <div> help page for script pdfbox.
+#  Source Path:
+#	pdfbox.cgi
+#  Source SHA1 Digest:
+#	No SHA1 Calculated
+#  Note:
+#	pdfbox.d/help.pl was generated automatically by cgi2perl5.
+#
+#	Do not make changes directly to this script.
+#
+
+our (%QUERY_ARG);
+
+print <<END;
+<div$QUERY_ARG{id_att}$QUERY_ARG{class_att}>
+END
+print <<'END';
+ <h1>Help Page for <code>/cgi-bin/pdfbox</code></h1>
+ <div class="overview">
+  <h2>Overview</h2>
+  <dl>
+<dt>Title</dt>
+<dd>/cgi-bin/pdfbox</dd>
+<dt>Synopsis</dt>
+<dd>HTTP CGI Script /cgi-bin/pdfbox</dd>
+<dt>Blame</dt>
+<dd>jmscott</dd>
+  </dl>
+ </div>
+ <div class="GET">
+  <h2><code>GET</code> Request.</h2>
+   <div class="out">
+    <div class="handlers">
+    <h3>Output Scripts in <code>$SERVER_ROOT/lib/pdfbox.d</code></h3>
+    <dl>
+     <dt>table</dt>
+     <dd>
+<div class="query-args">
+ <h4>Query Args</h4>
+ <dl>
+  <dt>q</dt>
+  <dd>
+   <ul>
+    <li><code>perl5_re:</code> .{0,255}</li>
+   </ul>
+  </dd>
+  <dt>tsconf</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> english</li>
+    <li><code>perl5_re:</code> [a-z0-9-]{1,32}</li>
+   </ul>
+  </dd>
+  <dt>rnorm</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> 14</li>
+    <li><code>perl5_re:</code> [0-9]{1,3}</li>
+   </ul>
+  </dd>
+  <dt>lim</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> 10</li>
+    <li><code>perl5_re:</code> [1-9][0-9]{0,3}</li>
+   </ul>
+  </dd>
+  <dt>offset</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> 0</li>
+    <li><code>perl5_re:</code> [+0-9]{1,10}</li>
+   </ul>
+  </dd>
+  </dl>
+</div>
+     </dd>
+     <dt>dl</dt>
+     <dd>
+<div class="query-args">
+ <h4>Query Args</h4>
+ <dl>
+  <dt>q</dt>
+  <dd>
+   <ul>
+    <li><code>perl5_re:</code> .{0,255}</li>
+   </ul>
+  </dd>
+  <dt>tsconf</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> english</li>
+    <li><code>perl5_re:</code> [a-z0-9-]{1,32}</li>
+   </ul>
+  </dd>
+  <dt>rnorm</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> 14</li>
+    <li><code>perl5_re:</code> [0-9]{1,3}</li>
+   </ul>
+  </dd>
+  <dt>lim</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> 10</li>
+    <li><code>perl5_re:</code> [1-9][0-9]{0,3}</li>
+   </ul>
+  </dd>
+  <dt>offset</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> 0</li>
+    <li><code>perl5_re:</code> [+0-9]{1,10}</li>
+   </ul>
+  </dd>
+  <dt>qtype</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> web</li>
+    <li><code>perl5_re:</code> web|key|phrase|fts</li>
+   </ul>
+  </dd>
+  </dl>
+</div>
+     </dd>
+     <dt>span.nav</dt>
+     <dd>
+<div class="query-args">
+ <h4>Query Args</h4>
+ <dl>
+  <dt>q</dt>
+  <dd>
+   <ul>
+    <li><code>perl5_re:</code> .{0,255}</li>
+   </ul>
+  </dd>
+  <dt>offset</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> 0</li>
+    <li><code>perl5_re:</code> [+0-9]{1,10}</li>
+   </ul>
+  </dd>
+  <dt>qtype</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> web</li>
+    <li><code>perl5_re:</code> web|key|phrase|fts</li>
+   </ul>
+  </dd>
+  </dl>
+</div>
+     </dd>
+     <dt>select.qtype</dt>
+     <dd>
+<div class="query-args">
+ <h4>Query Args</h4>
+ <dl>
+  <dt>qtype</dt>
+  <dd>
+   <ul>
+    <li><code>default:</code> web</li>
+    <li><code>perl5_re:</code> web|key|phrase|fts</li>
+   </ul>
+  </dd>
+  </dl>
+</div>
+     </dd>
+     <dt>input</dt>
+     <dd>
+<div class="query-args">
+ <h4>Query Args</h4>
+ <dl>
+  <dt>q</dt>
+  <dd>
+   <ul>
+    <li><code>perl5_re:</code> .{0,255}</li>
+   </ul>
+  </dd>
+  </dl>
+</div>
+     </dd>
+     <dt>mime.pdf</dt>
+     <dd>
+<div class="query-args">
+ <h4>Query Args</h4>
+ <dl>
+  <dt>udig</dt>
+  <dd>
+   <ul>
+    <li><code>perl5_re:</code> [a-z][a-z0-9]{0,7}:[[:graph:]]{32,128}</li>
+   </ul>
+  </dd>
+  </dl>
+</div>
+     </dd>
+  </dl>
+ </div>
+</div>
+<div class="examples">
+ <h3>Examples</h3>
+ <dl>
+   <dt><a href="/cgi-bin/pdfbox?/cgi-bin/pdfbox?help">/cgi-bin/pdfbox?/cgi-bin/pdfbox?help</a></dt>
+   <dd>Generate This Help Page for the CGI Script /cgi-bin/pdfbox</dd>
+ </dl>
+</div>
+ </div>
+</div>
+END
+
+1;
---
+++ www/mydash/pdfbox.d/span.nav.pl
@@ -0,0 +1,99 @@
+#
+#  Synopsis:
+#	Write html <span> of for navigating search results
+#  Note:
+#	One day this query may be a materialized view.
+#
+
+use utf8;
+
+#  stop apache log message 'Wide character in print at ...' from arrow chars
+binmode(STDOUT, ":utf8");
+
+require 'dbi-pg.pl';
+require 'pdfbox.d/common.pl';
+
+our %QUERY_ARG;
+
+my $q = $QUERY_ARG{q};
+my $offset = $QUERY_ARG{offset};
+my $qtype = $QUERY_ARG{qtype};
+my ($sql, $argv);
+
+if ($q =~ /[[:graph:]]/) {
+	$argv = [
+		$q,
+		'english'
+	];
+	my $to_tsquery = 'websearch_to_tsquery';
+
+	if ($qtype eq 'phrase') {
+		$to_tsquery = 'phraseto_tsquery'; 
+	} elsif ($qtype eq 'fts') {
+		$to_tsquery = 'to_tsquery';
+	} elsif ($qtype eq 'key') {
+		$to_tsquery = 'plainto_tsquery';
+	}
+	$sql =<<END;
+SELECT
+	count(DISTINCT tsv.pdf_blob) AS pdf_count,
+	count(tsv.pdf_blob)::float8 AS pdf_page_count
+  FROM
+	pdfbox.page_tsv_utf8 tsv,
+	$to_tsquery(\$2, \$1) AS q
+  WHERE
+  	tsv.tsv @@ q
+	AND
+	tsv.ts_conf = \$2::regconfig
+;
+END
+	;
+} else {
+	$sql = q(
+SELECT
+	count(pd.*) AS pdf_count,
+	sum(pd.number_of_pages) AS pdf_page_count
+  FROM
+  	pdfbox.pddocument pd
+	  JOIN setcore.byte_count bc ON (bc.blob = pd.blob)
+;);
+}
+
+my $r = dbi_pg_select(
+		db =>	dbi_pg_connect(),
+		tag =>	'select-span-stat',
+		argv =>	$argv,
+		sql => $sql
+)->fetchrow_hashref();
+
+my $pdf_count = $r->{pdf_count};
+my $pdf_page_count = $r->{pdf_page_count};
+
+print <<END;
+<span
+  $QUERY_ARG{id_att}
+  $QUERY_ARG{class_att}
+>
+END
+
+my $arrow_off;
+$q = encode_url_query_arg($q);
+if ($offset >= 10) {
+	$arrow_off = $offset - 10;
+	print <<END;
+<a href="/pdfbox.shtml?q=$q&offset=$arrow_off&qtype=$qtype">◀</a>
+END
+}
+
+print <<END;
+$pdf_count docs, $pdf_page_count pages
+END
+
+$arrow_off = $offset + 10;
+print <<END if $arrow_off < $pdf_count;
+<a href="/pdfbox.shtml?q=$q&offset=$arrow_off&qtype=$qtype">▶</a>
+END
+
+print <<END;
+</span>
+END
