77c780e5f2872f6b024ca84cc7e11966ecd6557b
jmscott <jmscott@setspace.com>
2019-09-03 17:43:11 -0500

    added DROP TABLE statements

--- schema/mydash/schema.sql
+++ schema/mydash/schema.sql
@@ -1,6 +1,6 @@
 /*
  *  Synopsis:
- *	PostgreSQL schema for data related to a user on www dash.setspace.com
+ *	PostgreSQL Schema of web pages tagged in a browser.
  */
 
 \set ON_ERROR_STOP 1
@@ -13,14 +13,12 @@ COMMENT ON SCHEMA mydash IS
   'Tables describing state of dashboard for a setspace user'
 ;
 
+DROP TABLE IF EXISTS tag_http CASCADE;
 CREATE TABLE tag_http
 (
 	blob		udig
 				REFERENCES setcore.service
 				PRIMARY KEY,
-	title		text CHECK (
-				length(title) < 1024
-			) NOT NULL,
 	--  the url must be normalized!
 	url		text CHECK (
 				length(url) < 1024
@@ -34,6 +32,62 @@ CREATE TABLE tag_http
 );
 COMMENT ON TABLE tag_http IS
   'Table of urls tagged in a browser in the dashboard'
+;
+
+DROP TABLE IF EXISTS tag_http_title CASCADE;
+CREATE TABLE tag_http_title
+(
+	blob		udig
+				REFERENCES tag_http
+				ON DELETE CASCADE
+				PRIMARY KEY,
+	title		text CHECK (
+				length(title) < 1024
+			) NOT NULL
+);
+COMMENT ON TABLE tag_http_title IS
+  'Extracted <title> from a tagged web page'
+;
+
+DROP TABLE IF EXISTS CASCADE;
+CREATE TABLE tag_http_title_tsv
+(
+	blob		udig
+				REFERENCES tag_http_title
+				ON DELETE CASCADE
+				PRIMARY KEY,
+	tsv		tsvector
+				NOT NULL
+);
+CREATE INDEX tag_http_title_tsv_rumidx ON tag_http_title_tsv
+  USING
+  	rum (tsv rum_tsvector_ops)
+;
+COMMENT ON TABLE tag_http_title IS
+  'Text Search Vector of title of a tagged web page'
+;
+
+CREATE OR REPLACE FUNCTION insert_tag_http_title_tsv() RETURNS TRIGGER AS
+  $$ begin
+	INSERT INTO tag_http_title_tsv(
+		blob,
+		tsv
+	) VALUES (
+		new.blob,
+		to_tsvector(new.title)
+	) ON CONFLICT
+		DO NOTHING
+	;
+  end $$
+  LANGUAGE plpgsql
+;
+COMMENT ON FUNCTION insert_tag_http_title_tsv IS
+  'Update text search vector for table tag_http_title_tsv'
+;
+
+DROP TRIGGER IF EXISTS insert_tag_http_title_tsv ON tag_http_title;
+CREATE TRIGGER insert_tag_http_title_tsv AFTER INSERT
+  ON tag_http_title FOR EACH ROW EXECUTE PROCEDURE insert_tag_http_title_tsv()
 ;
 
 COMMIT;
