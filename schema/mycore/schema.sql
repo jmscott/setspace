829840b6027a7aa5d522f12c6c4936e1e4cf060d
jmscott <jmscott@setspace.com>
2019-09-17 18:57:57 -0500

    added text search vector for title and notes

--- schema/mycore/schema.sql
+++ schema/mycore/schema.sql
@@ -25,12 +25,37 @@ CREATE TABLE title
 				AND
 				--  not all spaces
 				title ~ '[[:graph:]]'
-			) NOT NULL
+			) NOT NULL,
+	insert_time	timestamptz
+				DEFAULT NOW(),
+	update_time	timestamptz CHECK (
+				update_time >= insert_time
+			)
 );
 COMMENT ON TABLE title IS
 	'My title for any blob'
 ;
 CREATE INDEX idx_title ON title USING hash(blob);
+
+DROP TABLE IF EXISTS CASCADE;
+CREATE TABLE title_tsv
+(
+	blob		udig
+				REFERENCES title
+				ON DELETE CASCADE
+				PRIMARY KEY,
+	ts_conf		regconfig
+				NOT NULL,
+	tsv		tsvector
+				NOT NULL
+);
+CREATE INDEX title_tsv_rumidx ON title_tsv
+  USING
+  	rum (tsv rum_tsvector_ops)
+;
+COMMENT ON TABLE title_tsv IS
+  'Text Search Vector of title of a blob'
+;
 
 DROP TABLE IF EXISTS note;
 CREATE TABLE note
@@ -41,7 +66,7 @@ CREATE TABLE note
 	note	text	CHECK (
 				note ~ '[[:graph:]]'
 				AND
-				length(note) < 255
+				length(note) < 4096
 			) NOT NULL,
 	insert_time	timestamptz
 				DEFAULT NOW(),
@@ -50,8 +75,32 @@ CREATE TABLE note
 			) DEFAULT NOW() NOT NULL,
 	PRIMARY KEY	(blob, insert_time)
 );
-COMMENT ON TABLE title IS
+COMMENT ON TABLE note IS
 	'My notes for any blob'
+;
+
+DROP TABLE IF EXISTS CASCADE;
+CREATE TABLE note_tsv
+(
+	blob		udig,
+	insert_time	timestamptz,
+	ts_conf		regconfig
+				NOT NULL,
+	tsv		tsvector
+				NOT NULL,
+
+	PRIMARY KEY	(blob, insert_time, ts_conf),
+	FOREIGN KEY	(blob, insert_time)
+				REFERENCES note
+				ON DELETE CASCADE
+
+);
+CREATE INDEX note_tsv_rumidx ON note_tsv
+  USING
+  	rum (tsv rum_tsvector_ops)
+;
+COMMENT ON TABLE note_tsv IS
+  'Text Search Vector of a particular note of a blob'
 ;
 
 DROP TABLE IF EXISTS tags CASCADE;
