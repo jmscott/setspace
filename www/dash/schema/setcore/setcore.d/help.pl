5c13c4a437f3dffd9113c2f293630776cbbeae6d
John the Scott <jmscott@setspace.com>
2019-10-02 08:19:45 -0500

    recompile with new cgi2perl5

--- www/dash/schema/setcore/setcore.d/help.pl
+++ www/dash/schema/setcore/setcore.d/help.pl
@@ -141,7 +141,7 @@ print <<'END';
 <div class="examples">
  <h3>Examples</h3>
  <dl>
-   <dt><a href="/cgi-bin/jmscott/setcore?/cgi-bin/setcore?help">/cgi-bin/jmscott/setcore?/cgi-bin/setcore?help</a></dt>
+   <dt><a href="/cgi-bin/setcore?/cgi-bin/setcore?help">/cgi-bin/setcore?/cgi-bin/setcore?help</a></dt>
    <dd>Generate This Help Page for the CGI Script /cgi-bin/setcore</dd>
  </dl>
 </div>
