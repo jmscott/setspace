2ccc04e47699c059bd8731846a62f07224a5a68b
jmscott <jmscott@setspace.com>
2020-01-03 10:49:25 -0600

    added process execs

--- schema/mydash/trace-blob-flow.sql
+++ schema/mydash/trace-blob-flow.sql
@@ -4,19 +4,69 @@
  */
 
 \x
+\pset footer off
 
 SELECT
+	'Flow Detail' AS "Summary",
+	'           ' AS " ",
 	blob,
-	count(schema_name) AS schema_count,
-	sum(ok_count) AS ok_count,
-	sum(fault_count) AS fault_count,
-	avg(EXTRACT(epoch FROM wall_duration)) AS avg_wall_duration,
-	stddev(EXTRACT(epoch FROM wall_duration)) AS stddev_wall_duration,
-	MIN(EXTRACT(epoch FROM wall_duration)) AS min_wall_duration,
-	MAX(EXTRACT(epoch FROM wall_duration)) AS max_wall_duration
+	COUNT(schema_name) AS schema_count,
+	SUM(ok_count) AS ok_count,
+	SUM(fault_count) AS fault_count,
+	to_char(
+		AVG(EXTRACT(epoch FROM wall_duration)),
+		'FM999999.99'
+	) || ' sec' AS avg_wall_duration,
+	to_char(
+		STDDEV_POP(EXTRACT(epoch FROM wall_duration)),
+		'FM999999.99'
+	) || ' sec' AS stddev_pop_wall_duration,
+	MIN(EXTRACT(epoch FROM wall_duration)) || ' sec' AS min_wall_duration,
+	MAX(EXTRACT(epoch FROM wall_duration)) || ' sec' AS max_wall_duration
   FROM
   	mydash.trace_fdr
   GROUP BY
   	blob
+  ORDER BY
+  	blob ASC
 ;
 
+SELECT
+	'Process Execs' AS "Summary",
+	'' AS "                        ",
+	blob,
+	COUNT(command_name) AS command_count,
+
+	to_char(
+		AVG(EXTRACT(epoch FROM wall_duration)),
+		'FM99999.99'
+	) AS avg_wall_duration,
+	to_char(
+		STDDEV_POP(EXTRACT(epoch FROM wall_duration)),
+		'FM99999.99'
+	) AS stddev_pop_wall_duration,
+	to_char(
+		MAX(EXTRACT(epoch FROM wall_duration)),
+		'FM99999.99'
+	) AS max_wall_duration,
+
+	COUNT(termination_class)
+		FILTER (WHERE termination_class = 'OK')
+	  	AS OK_count,
+	COUNT(termination_class)
+		FILTER (WHERE termination_class = 'ERR')
+	  	AS ERR_count,
+	COUNT(termination_class)
+		FILTER (WHERE termination_class = 'SIG')
+	  	AS SIG_count,
+	COUNT(termination_class)
+		FILTER (WHERE termination_class = 'NOPS')
+	  	AS NOPS_count
+
+  FROM
+  	mydash.trace_xdr
+  GROUP BY
+  	blob
+  ORDER BY
+  	blob ASC
+;
