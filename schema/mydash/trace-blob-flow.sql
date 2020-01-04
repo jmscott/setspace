f69e0b5b5d89c0b8e9f0528615ce15f98ee614c1
jmscott <jmscott@setspace.com>
2020-01-03 23:57:17 -0600

    added qdr summaries

--- schema/mydash/trace-blob-flow.sql
+++ schema/mydash/trace-blob-flow.sql
@@ -10,9 +10,10 @@ SELECT
 	'Flow Detail' AS "Summary",
 	'           ' AS " ",
 	blob,
-	COUNT(schema_name) AS schema_count,
+	COUNT(DISTINCT schema_name) AS schema_count,
 	SUM(ok_count) AS ok_count,
 	SUM(fault_count) AS fault_count,
+
 	to_char(
 		AVG(EXTRACT(epoch FROM wall_duration)),
 		'FM999999.99'
@@ -21,8 +22,14 @@ SELECT
 		STDDEV_POP(EXTRACT(epoch FROM wall_duration)),
 		'FM999999.99'
 	) || ' sec' AS stddev_pop_wall_duration,
-	MIN(EXTRACT(epoch FROM wall_duration)) || ' sec' AS min_wall_duration,
-	MAX(EXTRACT(epoch FROM wall_duration)) || ' sec' AS max_wall_duration
+	to_char(
+		MIN(EXTRACT(epoch FROM wall_duration)),
+		'FM999999.99'
+	) || ' sec' AS min_wall_duration,
+	to_char(
+		MAX(EXTRACT(epoch FROM wall_duration)),
+		'FM999999.99'
+	) || ' sec' AS max_wall_duration
   FROM
   	mydash.trace_fdr
   GROUP BY
@@ -35,7 +42,8 @@ SELECT
 	'Process Execs' AS "Summary",
 	'' AS "                        ",
 	blob,
-	COUNT(command_name) AS command_count,
+	COUNT(DISTINCT schema_name) AS schema_count,
+	COUNT(DISTINCT command_name) AS command_count,
 
 	to_char(
 		AVG(EXTRACT(epoch FROM wall_duration)),
@@ -65,6 +73,45 @@ SELECT
 
   FROM
   	mydash.trace_xdr
+  GROUP BY
+  	blob
+  ORDER BY
+  	blob ASC
+;
+
+SELECT
+	'Queries' AS "Summary",
+	'' AS "                        ",
+	blob,
+	COUNT(DISTINCT schema_name) AS schema_count,
+	COUNT(DISTINCT query_name) AS query_count,
+
+	to_char(
+		AVG(EXTRACT(epoch FROM wall_duration)),
+		'FM99999.99'
+	) AS avg_wall_duration,
+
+	to_char(
+		STDDEV_POP(EXTRACT(epoch FROM wall_duration)),
+		'FM99999.99'
+	) AS stddev_pop_wall_duration,
+
+	to_char(
+		MAX(EXTRACT(epoch FROM wall_duration)),
+		'FM99999.99'
+	) AS max_wall_duration,
+
+	COUNT(DISTINCT sqlstate) AS sqlstate_count,
+	COUNT(termination_class)
+		FILTER (WHERE termination_class = 'OK')
+	  	AS OK_count,
+	COUNT(termination_class)
+		FILTER (WHERE termination_class = 'ERR')
+	  	AS ERR_count,
+	SUM(rows_affected) AS rows_affected_total
+
+  FROM
+  	mydash.trace_qdr
   GROUP BY
   	blob
   ORDER BY
