4dda247fabf4703dbdba3618f8747cc8d5e7c480
jmscott <jmscott@setspace.com>
2020-09-22 19:55:17 -0500

    checkpoint

--- schema/arxiv/candidate.sql
+++ schema/arxiv/candidate.sql
@@ -2,37 +2,36 @@
 
 SELECT
 	p.pdf_blob AS "PDF",
-	pd.number_of_pages AS "Pages",
-	substring(p.txt FROM candidate.re) AS "Arxiv ID"
+	(regexp_match(p.txt, candidate.re, 'is'))[1] AS "Arxiv Id"
   FROM
   	pdfbox.page_text_utf8 p
-	  LEFT OUTER JOIN mycore.title t ON (t.blob = p.pdf_blob),
-	pdfbox.pddocument pd,
+	  LEFT OUTER JOIN mycore.title t ON (t.blob = p.pdf_blob)
+	  JOIN pdfbox.pddocument pd ON (
+	  	pd.blob = p.pdf_blob
+	  ),
 	(VALUES (
-		--  syntax for 2007 arxiv id derived from page
-		--  https://arxiv.org/help/arxiv_identifier
+	 '\m('							||
+	 	--  variations on "arxiv:"
+		'\s*a\s*r\s*x\s*i\s*v\s*:'			||
 
-		--  word arXiv: with spaces between chars
-	  	'\m(a\s*r\s*x\s*i\s*v\s*:\s*'				||
+		--  variations on YYMM.
+		'\s*[0-9]\s*[0-9]\s*[0-3]\s*[0-9]\s*\.'		||
 
-		--  YYMM.
-		'[0-9]\s*[0-9]\s*[0-3]\s*[0-9]\s*[.]\s*'		||
+		--  variations on the 4 or 5 digit article monthly index
+		'(\s*[0-9]){4,5}'				||
 
-		--  sequence of 4 or 5 digits
-		'([0-9]\s*){4,5}'					||
-
-		-- version number
-		'v\s*[1-9]{1,3})'
-
+		--  variation on article version v9
+		'\s*v\s*[0-9](\s*[0-9])?'
+	 ')\M'
 	)) AS candidate(re)
   WHERE
   	p.txt ~* candidate.re
 	AND
 	p.page_number = 1
 	AND
-	t.blob IS null
-	AND
-	pd.blob = p.pdf_blob
+	t.blob IS NULL
   ORDER BY
-  	random()
+	"PDF" DESC
+  LIMIT
+  	10
 ;
