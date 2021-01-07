559e8139b795996e9f1b932bcc4a0e73699bb238
jmscott <jmscott@setspace.com>
2021-01-06 18:27:14 -0600

    replaced prefix test with list test

--- schema/gnuzip/rummy.sql
+++ schema/gnuzip/rummy.sql
@@ -3,15 +3,12 @@
  *	Find recent blobs in service but not in a table in gnufile.*
  */
 SELECT
-	DISTINCT s.blob
+	t.blob
   FROM
-  	setcore.service s
-  	  JOIN setcore.byte_prefix_32 p ON (p.blob = s.blob)
-	  LEFT OUTER JOIN gnuzip.gunzip_test_quiet g ON (g.blob = p.blob)
+  	gnuzip.gunzip_test_quiet t
+	  LEFT OUTER JOIN gnuzip.gunzip_list_verbose v ON (v.blob = t.blob)
   WHERE
-	g.blob IS NULL
+  	t.exit_status = 0
 	AND
-	s.discover_time between (now() + :since) AND (now() + '-1 minute')
-	AND
-	substr(p.prefix, 1, 2) = '\x1F8B'
+	v.blob IS NULL
 ;
