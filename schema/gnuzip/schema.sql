9bca829f2b7491f348fac4079f2bfc8e2a4770c3
jmscott <jmscott@setspace.com>
2021-01-08 17:31:18 -0600

    constraint function gunzip_test_exit_status()

--- schema/gnuzip/schema.sql
+++ schema/gnuzip/schema.sql
@@ -17,6 +17,7 @@ CREATE SCHEMA gnuzip;
 
 SET search_path to gnuzip,public;
 
+DROP TABLE IF EXISTS gunzip_test;
 CREATE TABLE gunzip_test
 (
 	blob		udig
@@ -27,19 +28,48 @@ CREATE TABLE gunzip_test
 				NOT NULL
 );
 COMMENT ON TABLE gunzip_test IS
-  'Exit status of gnu command "gunzip --test --quiet"'
+  'Exit status of gnu command "gunzip --test"'
+;
+
+/*
+ *  Integrity tests need to insure exit_status == 0
+ *  for child tables.  Perhaps a inherited table is better
+ *  approach.
+ */
+DROP FUNCTION IF EXISTS gunzip_test_exit_status(udig)
+  CASCADE
+; 
+CREATE FUNCTION gunzip_test_exit_status(blob udig)
+  RETURNS setcore.uni_xstatus
+  AS $$
+  	SELECT
+		exit_status
+	  FROM
+	  	gnuzip.gunzip_test
+	  WHERE
+	  	blob = $1
+  $$ LANGUAGE SQL
+; 
+COMMENT ON FUNCTION gunzip_test_exit_status(udig) IS
+  'Fetch the exit status in table gunzip_test'
 ;
 
 /*
  *  Output of gunzip (v1.10) --list --name
  *	
  */
+DROP TABLE IF EXISTS gunzip_uncompressed_name;
 CREATE TABLE gunzip_uncompressed_name
 (
 	blob		udig
 				REFERENCES gunzip_test
+				ON DELETE CASCADE
 				PRIMARY KEY,
-	name		text NOT NULL
+	name		text NOT NULL,
+
+	CONSTRAINT exit_status_0 CHECK (
+		gnuzip.gunzip_test_exit_status(blob) = 0
+	)
 );
 COMMENT ON TABLE gunzip_uncompressed_name IS
   'Uncompressed file name extracted from gunzip --list --name'
@@ -51,13 +81,21 @@ COMMENT ON TABLE gunzip_uncompressed_name IS
  *  Note:
  *	32bit file sizes make gunzip unreliable.
  */
-DROP TABLE IF EXISTS gnuzip_uncompress_byte_count;
-CREATE TABLE gnuzip_uncompress_byte_count
+DROP TABLE IF EXISTS gnuzip_uncompressed_byte_count;
+CREATE TABLE gnuzip_uncompressed_byte_count
 (
 	blob		udig
 				REFERENCES gunzip_test
+				ON DELETE CASCADE
 				PRIMARY KEY,
-	byte_count	bigint
+	byte_count	bigint,
+
+	CONSTRAINT exit_status_0 CHECK (
+		gnuzip.gunzip_test_exit_status(blob) = 0
+	)
 );
+COMMENT ON TABLE gnuzip_uncompressed_byte_count IS
+  'Correct, uncompressed file size calculated "gunzip --stdout | setcore/byte-count'
+;
 
 COMMIT;
