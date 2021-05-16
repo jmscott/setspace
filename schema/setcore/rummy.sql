3197135950afe9d315bfd5bee88e7a3e4da3d5f2
jmscott <jmscott@setspace.com>
2021-05-16 16:57:59 -0500

    made "since" var unescaped

--- schema/setcore/rummy.sql
+++ schema/setcore/rummy.sql
@@ -2,7 +2,7 @@
  *  Synopsis:
  *	Summarize tables with incomplete information since a point in time.
  *  Usage:
- *	psql -f rummy.sql
+ *	psql -f rummy.sql --var since='-1 day'
  *  Blame:
  *  	jmscott@setspace.com
  *  	setspace@gmail.com
@@ -19,7 +19,7 @@ SELECT
 	  LEFT OUTER JOIN byte_suffix_32 bs32 ON (bs32.blob = s.blob)
 	  LEFT OUTER JOIN is_utf8wf u8 ON (u8.blob = s.blob)
   where
-  	s.discover_time >= now() + :since
+  	s.discover_time >= now() + :'since'
 	AND
 	(
 		bc.blob IS NULL
