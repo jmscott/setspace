adbf937d6344270ceb435ef8d77bc94e9e204422
John the Scott <jmscott@setspace.com>
2022-02-18 17:38:32 -0600

    first cut at fffil5 web gui

---
+++ www/dash/schema/fffile5/fffile5.cgi
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<cgi name="fffile5">
+ <title>/cgi-bin/schema/fffile5</title>
+ <synopsis>HTTP CGI Script /cgi-bin/schema/fffile5</synopsis>
+ <blame>jmscott</blame>
+ <GET>
+  <examples>
+   <example
+   	query="/cgi-bin/schema/fffile5?help"
+   >
+    Generate This Help Page for the CGI Script /cgi-bin/schema/fffile5
+   </example>
+  </examples>
+  <out content-type="text/html">
+
+   <putter name="select"></putter>
+  </out>
+ </GET>
+
+</cgi>
---
+++ www/dash/schema/fffile5/fffile5.d/help.pl
@@ -0,0 +1,56 @@
+#
+#  Synopsis:
+#	Write <div> help page for script fffile5.
+#  Source Path:
+#	fffile5.cgi
+#  Source SHA1 Digest:
+#	No SHA1 Calculated
+#  Note:
+#	fffile5.d/help.pl was generated automatically by cgi2perl5.
+#
+#	Do not make changes directly to this script.
+#
+
+our (%QUERY_ARG);
+
+print <<END;
+<div$QUERY_ARG{id_att}$QUERY_ARG{class_att}>
+END
+print <<'END';
+ <h1>Help Page for <code>/cgi-bin/schema/fffile5</code></h1>
+ <div class="overview">
+  <h2>Overview</h2>
+  <dl>
+<dt>Title</dt>
+<dd>/cgi-bin/schema/fffile5</dd>
+<dt>Synopsis</dt>
+<dd>HTTP CGI Script /cgi-bin/schema/fffile5</dd>
+<dt>Blame</dt>
+<dd>jmscott</dd>
+  </dl>
+ </div>
+ <div class="GET">
+  <h2><code>GET</code> Request.</h2>
+   <div class="out">
+    <div class="handlers">
+    <h3>Output Scripts in <code>$SERVER_ROOT/lib/schema/fffile5.d</code></h3>
+    <dl>
+     <dt>select</dt>
+     <dd>
+     </dd>
+  </dl>
+ </div>
+</div>
+<div class="examples">
+ <h3>Examples</h3>
+ <dl>
+   <dt><a href="/cgi-bin/schema/fffile5?/cgi-bin/schema/fffile5?help">
+fffile5?/cgi-bin/schema/fffile5?help</a></dt>
+   <dd>Generate This Help Page for the CGI Script /cgi-bin/schema/fffile5</dd>
+ </dl>
+</div>
+ </div>
+</div>
+END
+
+1;
