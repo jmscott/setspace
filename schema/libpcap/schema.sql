c0d2f5ed7cf87fc11378a2a493a505049d682aca
John the Scott <jmscott@setspace.com>
2022-04-14 18:05:57 -0500

    added CASCADE to DROP SCHEMA

---
+++ schema/libpcap/schema.sql
@@ -0,0 +1,27 @@
+/*
+ *  Synopsis:
+ *	Schema for processing libpcap blobs.
+ */
+
+\set ON_ERROR_STOP true
+SET search_path TO libpcap,public;
+
+BEGIN TRANSACTION;
+
+DROP SCHEMA IF EXISTS libpcap CASCADE;
+CREATE SCHEMA libpcap;
+COMMENT ON SCHEMA libpcap IS
+  'Details concerning libpcal blobs'
+;
+
+CREATE VIEW rummy AS
+  SELECT
+  	'btc20:fd7b15dc5dc2039556693555c2b81b36c8deec15'::udig
+    WHERE
+    	false
+;
+COMMENT ON VIEW rummy IS
+  'Known unknown blobs in schema libpcap'
+;
+
+COMMIT TRANSACTION;
