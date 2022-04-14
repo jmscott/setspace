81b0757278ac22d6f3de51738afa6f11e1c8782e
John the Scott <jmscott@setspace.com>
2022-04-14 18:04:15 -0500

    added prefixio schema with view rummy

---
+++ schema/prefixio/schema.sql
@@ -0,0 +1,27 @@
+/*
+ *  Synopsis:
+ *	Schema for routing based upon json patterns.
+ */
+
+\set ON_ERROR_STOP true
+SET search_path TO prefixio,public;
+
+BEGIN TRANSACTION;
+
+DROP SCHEMA IF EXISTS prefixio CASCADE;
+CREATE SCHEMA prefixio;
+COMMENT ON SCHEMA prefixio IS
+  'Routing rules based upon the first 32 bytes of the blob'
+;
+
+CREATE VIEW rummy AS
+  SELECT
+  	'btc20:fd7b15dc5dc2039556693555c2b81b36c8deec15'::udig
+    WHERE
+    	false
+;
+COMMENT ON VIEW rummy IS
+  'Known unknown blobs in schema prefixio'
+;
+
+COMMIT TRANSACTION;
