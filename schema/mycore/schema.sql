9d529b622279779a8ace12f585128890ca3d4396
John the Scott <jmscott@setspace.com>
2024-12-18 23:09:06 -0600

    stable snapshot of complete rewrite

--- schema/mycore/schema.sql
+++ schema/mycore/schema.sql
@@ -1,36 +1,22 @@
 /*
  *  Synopsis:
- *	My personal metadata about blobs, like title, notes and tags.
- *  Schema:
+ *	My personal metadata about any blob, like title, notes and tags.
+ *  Schemas:
  *	setcore,jsonorg
  *  PG Extensions:
- *	pg_trgm, rum
+ *	rum@github/postgrespro
  *  Note:
- *	No explcit binding to a particular user other than via table
- *	mycore.service!  At some point we may need a table
- *	mycore.json_service_request.  Or, perhaps row level security is the
- *	path.
- *
- *	Really, really, really need json schema!
- *
- *	Not clear if title_request.core_blob should point to title.blob
- *	or just point to setcore.title or even exist at all.  In other words,
- *	should table title_request simply summarize the json, with no
- *	implications of existence of the blob?  Does the exitence of
- *	and entry in table title imply an entry in table title_request ...
- *	probably not.  not sure.
- *	
- *	What is the unicode generalization of space and vertical breaks?
+ *	thunk deeper about table title_tsv with pk(blob, regconfig) versus
+ *	pk(blob).
  */
 \set ON_ERROR_STOP on
-\timing
 
 BEGIN TRANSACTION;
 
 DROP SCHEMA IF EXISTS mycore CASCADE;
 CREATE SCHEMA mycore;
 COMMENT ON SCHEMA mycore IS
-  'My curated metadata about core blobs, like title, notes and tags'
+  'My curated, text searchable metadata (title, tag, note, etc) of core blobs'
 ;
 SET search_path TO mycore,public;
 
@@ -38,43 +24,21 @@ DROP DOMAIN IF EXISTS title_255 CASCADE;
 CREATE DOMAIN title_255 AS text CHECK (
 	length(value) < 256
 	AND
-	value ~ '[[:graph:]]'		--  at least something to display
-	AND
-	value !~ '^[[:space:]]'		--  no leading space
-	AND
-	value !~ '[[:space:]]$'		--  no trailing space
-	AND
-	value !~ '[\t\n\f\r]'		--  no tab/newline/formfeed/carriage
-	AND
-	value !~ '[[:space:]][[:space:]]'	--  no multiple spaces
-);
-
-DROP TABLE IF EXISTS service CASCADE;
-CREATE TABLE service
-(
-	blob	udig
-			REFERENCES setcore.service
-			ON DELETE CASCADE
-			PRIMARY KEY,
-	insert_time	setcore.inception NOT NULL
-			  default now()
+	length(value) > 0
 );
 
 DROP TABLE IF EXISTS title;
 CREATE TABLE title
 (
 	blob		udig
-				REFERENCES service
+				REFERENCES setcore.service
 				ON DELETE CASCADE
 				PRIMARY KEY,
 	title		title_255
-				NOT NULL,
-
-	insert_time	setcore.inception NOT NULL
-				DEFAULT now()
+				NOT NULL
 );
 COMMENT ON TABLE title IS
-  'My title for any blob'
+  'My title for any core blob'
 ;
 CREATE INDEX idx_title ON title USING hash(blob);
 
@@ -98,172 +62,41 @@ COMMENT ON TABLE title_tsv IS
   'Text Search Vector of title of a blob'
 ;
 
-/*
- *  A json request to modify the title of a blob.
- */
-DROP TABLE IF EXISTS json_title_merge;
-CREATE TABLE json_title_merge
+DROP TABLE IF EXISTS jsonorg_merge_request;
+CREATE TABLE jsonorg_merge_request
 (
 	request_blob	udig
 				REFERENCES jsonorg.jsonb_255(blob)
 				ON DELETE CASCADE
 				PRIMARY KEY,
 	core_blob	udig
-				REFERENCES service(blob)
-				ON DELETE CASCADE
+				REFERENCES setcore.service(blob)
+				ON DELETE CASCADE,
+	request_time	setcore.inception NOT NULL,
+	insert_time	setcore.inception NOT NULL DEFAULT now()
 );
-COMMENT ON TABLE json_title_merge IS
-  'JSON Request to change the title of blob'
+COMMENT ON TABLE jsonorg_merge_request IS
+  'JSONORG Request to insert or update title, tags, notes for list of blobs'
 ;
 
 /*
  *  A json request to delete the title of a blob.
  */
-DROP TABLE IF EXISTS json_title_delete;
-CREATE TABLE json_title_delete
+DROP TABLE IF EXISTS jsonorg_delete_request;
+CREATE TABLE jsonorg_delete_request
 (
 	request_blob	udig
 				REFERENCES jsonorg.jsonb_255(blob)
 				ON DELETE CASCADE
 				PRIMARY KEY,
 	core_blob	udig
-				REFERENCES service(blob)
-				ON DELETE CASCADE
+				REFERENCES setcore.service(blob)
+				ON DELETE CASCADE,
+	request_time	setcore.inception NOT NULL,
+	insert_time	setcore.inception NOT NULL DEFAULT now()
 );
-COMMENT ON TABLE json_title_merge IS
-  'JSON Request to change the title of blob'
-;
-
-DROP TABLE IF EXISTS note;
-CREATE TABLE note
-(
-	blob	udig
-			REFERENCES service(blob)
-			ON DELETE CASCADE,
-	insert_time	timestamptz
-				DEFAULT NOW(),
-	note	text	CHECK (
-				note ~ '[[:graph:]]'
-				AND
-				length(note) < 4096
-			) NOT NULL,
-	PRIMARY KEY	(blob, insert_time)
-);
-COMMENT ON TABLE note IS
-  'My notes for any blob'
-;
-
-DROP TABLE IF EXISTS CASCADE;
-CREATE TABLE note_tsv
-(
-	blob		udig,
-	insert_time	timestamptz,
-	ts_conf		regconfig
-				NOT NULL,
-	tsv		tsvector
-				NOT NULL,
-
-	PRIMARY KEY	(blob, insert_time, ts_conf),
-	FOREIGN KEY	(blob, insert_time)
-				REFERENCES note
-				ON DELETE CASCADE
-
-);
-CREATE INDEX note_tsv_rumidx ON note_tsv
-  USING
-  	rum (tsv rum_tsvector_ops)
-;
-COMMENT ON TABLE note_tsv IS
-  'Text Search Vector of a particular note of a blob'
-;
-
-DROP TABLE IF EXISTS tags CASCADE;
-CREATE TABLE tags
-(
-        tag     text    CHECK (
-                                tag ~ '[[:alpha:]][_[:alnum:]]{0,31}$'
-				and
-                                tag ~ '^[[:upper:]]{1,32}$'
-                        )
-                        PRIMARY KEY
-);
-COMMENT ON TABLE tags IS
-  'All my tags for any of my blobs'
-;
-
-/*
- *  A json request to merge tags into tabs tabs.
- */
-DROP TABLE IF EXISTS json_tags_merge;
-CREATE TABLE json_tags_merge
-(
-	request_blob	udig
-				REFERENCES jsonorg.jsonb_255(blob)
-				ON DELETE CASCADE
-				PRIMARY KEY
-);
-COMMENT ON TABLE json_tags_merge IS
-  'JSON Request to merge new tags into table tabs'
-;
-
-/*
- *  A json request to delete from table "tags".
- */
-DROP TABLE IF EXISTS json_tags_merge;
-CREATE TABLE json_tags_delete
-(
-	request_blob	udig
-				REFERENCES jsonorg.jsonb_255(blob)
-				ON DELETE CASCADE
-				PRIMARY KEY
-);
-COMMENT ON TABLE json_tags_delete IS
-  'JSON Request to delete tags into table tabs'
-;
-
-DROP TABLE IF EXISTS tag;
-CREATE TABLE tag
-(
-        blob    udig
-                        REFERENCES service
-                        ON DELETE CASCADE,
-        tag     text    REFERENCES tags
-                        ON DELETE CASCADE,
-        PRIMARY KEY     (blob, tag),
-	UNIQUE		(tag, blob)
-);
-COMMENT ON TABLE tags IS
-        'Tags for my blobs'
-;
-
-/*
- *  A json request to merge tags into "tab" table.
- */
-DROP TABLE IF EXISTS json_tab_merge;
-CREATE TABLE json_tab_merge
-(
-	request_blob	udig
-				REFERENCES jsonorg.jsonb_255(blob)
-				ON DELETE CASCADE
-				PRIMARY KEY
-);
-COMMENT ON TABLE json_tab_merge IS
-  'JSON Request to merge tag into table tab for a particular blob'
-;
-
-/*
- *  A json request to delete from table "tag".
- */
-DROP TABLE IF EXISTS json_tab_merge;
-CREATE TABLE json_tab_delete
-(
-	request_blob	udig
-				REFERENCES jsonorg.jsonb_255(blob)
-				ON DELETE CASCADE
-				PRIMARY KEY
-);
-COMMENT ON TABLE json_tab_delete IS
-  'JSON Request to delete all rows for a particular tab in table tag'
+COMMENT ON TABLE jsonorg_delete_request IS
+  'JSON Request to delete title,tags,notes for list of blobs'
 ;
 
 COMMIT TRANSACTION;
