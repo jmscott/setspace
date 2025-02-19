e6a0f29715dafb129802b69868eeb335152ccf40
John the Scott <jmscott@setspace.com>
2025-02-19 17:39:18 -0600

    zapped debug echo queries

--- schema/setcore/upsert-byte_prefix_32.sql
+++ schema/setcore/upsert-byte_prefix_32.sql
@@ -1,9 +1,11 @@
 /*
  *  Synopsis:
  *	Upsert a prefix into table setcore.byte_prefix_32
+ *  Usage:
+ *	BLOB=sha:a7a19726b95c73f8fa647df3562c8f00ef6ea338
+ *	PREFIX=732200a200a3436352e32286429283229206578656d70747320627573696e65
+ *	psql --set blob=$BLOB --set hex=$PREFIX
  */
-\set ECHO queries
-
 INSERT INTO setcore.byte_prefix_32 (
 	blob,
 	prefix
