9a3f2a4a00e73ca866df25b6534dbb0001619c4f
John the Scott <jmscott@setspace.com>
2025-02-19 20:34:55 -0600

    tweaked faults

--- schema/setcore/ssq-setcore-fault.sql
+++ schema/setcore/ssq-setcore-fault.sql
@@ -4,12 +4,16 @@
  */
 
 SELECT
-	NULL::udig AS blob,
-	NULL::text AS monitor__name,
-	NULL::text AS command_name,
-	NULL::text AS exit_class,
-	NULL::smallint AS exit_status,
-	NULL::smallint AS signal
-  WHERE
-	false
+	blob,
+	monitor_name,
+	command_name,
+	exit_class,
+	exit_status,
+	signal
+  FROM
+	setcore.fault
+  ORDER BY
+  	blob ASC,
+	monitor_name,
+	command_name
 ;
