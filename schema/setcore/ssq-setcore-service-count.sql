e6d81b4edc8066ac47d0cafc5865229533e47f32
John the Scott <jmscott@setspace.com>
2025-03-10 12:35:03 -0500

    added count of service

---
+++ schema/setcore/ssq-setcore-service-count.sql
@@ -0,0 +1,11 @@
+/*
+ *  Synopsis:
+ *	List blobs in table setcore.rummy, in no particular order.
+ *  Usage:
+ *	See script setcore/libexec/ssq-setcore-rummy
+ */
+SELECT
+	count(blob)
+  FROM
+  	setcore.service
+;
