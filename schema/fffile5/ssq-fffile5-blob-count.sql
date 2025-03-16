5ce273b82a4c26ac2500793fe54591bf95eeadd9
John the Scott <jmscott@setspace.com>
2025-03-16 16:52:26 -0500

    minor doc changes

--- schema/fffile5/ssq-fffile5-blob-count.sql
+++ schema/fffile5/ssq-fffile5-blob-count.sql
@@ -1,8 +1,8 @@
 /*
  *  Synopsis:
- *	Count blobs in table fffile5.blob.
+ *	Count blobs in table fffile5.blob1
  *  Usage:
- *	#  called by script ssq-fffile5-blob-count
+ *	#  called by script ssq-fffile5-fault-count
  *	psql --file ssq-fffile5-blob-count.sql
  */
 SELECT
