893e42a164135c5977684c061dcd6ed42972cf70
John the Scott <jmscott@setspace.com>
2025-03-16 20:45:45 -0500

    added action "frisk ls"

---
+++ schema/fffile5/ssq-fffile5-fault-ls.sql
@@ -0,0 +1,22 @@
+/*
+ *  Synopsis:
+ *	List blobs in table fffile5.fault.
+ *  Usage:
+ *	#  called by script ssq-fffile5-fault-ls
+ *	psql --file ssq-fffile5-fault-ls.sql
+ */
+SELECT
+	fault_time::text AS "Fault Time",
+	'flowd' AS "Process Class",
+        blob AS "Blob",
+	command_name AS "Command Name",
+	exit_class AS "Exit Class",
+	exit_status AS "Exit Status",
+	signal AS "Signal"
+  FROM
+        setops.flowd_call_fault
+  ORDER BY
+  	fault_time DESC,
+	blob ASC,
+	command_name ASC
+;
