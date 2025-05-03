5386aaa23bc9b8600f1b258f6adfcc9e2633a7cf
John the Scott <jmscott@setspace.com>
2025-05-02 21:28:07 -0500

    added views {blb,fault,service}

--- schema/prefixio/schema.sql
+++ schema/prefixio/schema.sql
@@ -4,6 +4,7 @@
  */
 
 \set ON_ERROR_STOP true
+
 SET search_path TO prefixio,setspace;
 
 BEGIN TRANSACTION;
@@ -14,14 +15,44 @@ COMMENT ON SCHEMA prefixio IS
   'Routing rules based upon the first 32 bytes of the blob'
 ;
 
+CREATE VIEW blob AS
+  SELECT
+  	null::udig
+    WHERE
+    	false
+;
+COMMENT ON VIEW blob IS
+  'Known unknown blobs (always empty)'
+;
+
 CREATE VIEW rummy AS
   SELECT
-  	'btc20:fd7b15dc5dc2039556693555c2b81b36c8deec15'::udig AS blob
+  	null::udig
     WHERE
     	false
 ;
 COMMENT ON VIEW rummy IS
-  'Known unknown blobs in schema prefixio'
+  'Known unknown blobs (always empty)'
+;
+
+CREATE VIEW service AS
+  SELECT
+  	null::udig
+    WHERE
+    	false
+;
+COMMENT ON VIEW service IS
+  'Blobs in service (always empty)'
+;
+
+CREATE VIEW fault AS
+  SELECT
+  	null::udig
+    WHERE
+    	false
+;
+COMMENT ON VIEW fault IS
+  'Blobs in fault (always empty)'
 ;
 
 COMMIT TRANSACTION;
