41b2da6153959f4afcf1ea8db453868ffd72d16a
John the Scott <jmscott@setspace.com>
2025-05-19 17:04:39 -0500

    refactor, including only requests in blob table

--- schema/mycore/upsert-request_title.sql
+++ schema/mycore/upsert-request_title.sql
@@ -3,11 +3,16 @@
  *	Honor a request to upsert titles into table mycore.title.
  *  Usage:
  *	REQUEST_BLOB=btc20:338a90467b9912326b021872f6d7db852539b8e0
- *	psql --var blob=$REQUEST_BLOB --f 
+ *	psql --var blob=$REQUEST_BLOB --file upsert-request_title.sql
+ *  Note:
+ *	is readiing json blob from stdin the best method?  perhaps replace
+ *	`cat` with `blobio get ...`.  Not sure.  Got to be a better method in
+ *	postgresql to insert json then slurping into a psql variable.
  */
 \set ON_ERROR_STOP 1
 
-\echo Loading json from stdin
+--  read json blob from stdin into psql variable 'j'
+
 \set j `cat`
 \set ts_conf 'english'
 
@@ -64,21 +69,10 @@ UPDATE load_title lt1
       FROM
       	load_title lt2
       WHERE
-      		lt2.blob = lt1.blob
-		AND
-		lt2.title != lt1.title
+	lt2.blob = lt1.blob
+	AND
+	lt2.title != lt1.title
     )
-;
-
-INSERT INTO blob
-  SELECT
-  	blob
-    FROM
-    	load_title
-    WHERE
-    	NOT has_dup
-    ON CONFLICT
-    	DO NOTHING
 ;
 
 INSERT INTO title(blob, title)
@@ -110,6 +104,49 @@ INSERT INTO title_tsv(blob, ts_conf, tsv)
 	tsv = EXCLUDED.tsv
     WHERE
 	title_tsv.tsv != EXCLUDED.tsv
+;
+
+--  fetch count of blobs in request set
+SELECT
+	count(DISTINCT blob) AS request_blob_count,
+	CASE WHEN count(DISTINCT blob) = 1 THEN '' ELSE 's' END AS plurality
+  FROM
+  	load_title
+;
+\gset
+
+--  add title for the json request blob itself
+
+INSERT INTO title(blob, title)
+  SELECT
+  	:'blob',
+	'mycore JSON Request to Insert Titles of Set of '		||
+		:'request_blob_count' || ' Blob' || :'plurality'
+  ON CONFLICT
+  	DO NOTHING
+;
+INSERT INTO title_tsv(blob, ts_conf, tsv)
+  SELECT
+  	:'blob',
+	:'ts_conf',
+	to_tsvector(:'ts_conf'::regconfig, title)
+    FROM
+    	title
+    WHERE
+    	blob = :'blob'
+  ON CONFLICT
+    	DO NOTHING
+;
+
+INSERT INTO request_title(blob, on_conflict, request_time)
+  SELECT
+  	:'blob',
+	'do_update',
+	to_timestamp((doc->>'request_unix_epoch')::double precision)
+    FROM
+	load_request doc
+  ON CONFLICT
+  	DO NOTHING
 ;
 
 END TRANSACTION;
