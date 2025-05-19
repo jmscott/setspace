addee278a3b4ffbe93a90fd42c5cccab98bd3fd6
John the Scott <jmscott@setspace.com>
2025-05-18 21:33:46 -0500

    stable bulk title updater

--- schema/mycore/upsert-request_title.sql
+++ schema/mycore/upsert-request_title.sql
@@ -1,50 +1,100 @@
 /*
  *  Synopsis:
- *	Honor a request to upsert titles in myair.title.
+ *	Honor a request to upsert titles into table mycore.title.
  *  Usage:
  *	REQUEST_BLOB=btc20:338a90467b9912326b021872f6d7db852539b8e0
  *	psql --var blob=$REQUEST_BLOB --f 
  */
 \set ON_ERROR_STOP 1
+
+\echo Loading json from stdin
 \set j `cat`
-select jsonb_pretty(:'j'::jsonb);
-\q
+
+SET search_path TO mycore,setspace;
+
+BEGIN TRANSACTION;
+
+INSERT INTO blob(blob)
+  VALUES (:'blob')
+  ON CONFLICT
+  DO NOTHING
+;
 
 CREATE TEMP TABLE load_request
 (
 	doc	jsonb
 );
---  read the JSON blob into a temp table 
-\copy load_request from pstdin 
-
-/*
- *  Note:
- *	How to order updates?
- */
-WITH req_doc AS (
-  SELECT
-  	jsonb_array_elements(doc->'upsert'->'titles') AS doc
-    FROM
-    	load_request
-    WHERE
-        blob = :'blob'
-), titles AS (
-  SELECT
-  	doc->>'title' AS title,
-  	(doc->>'blob')::udig AS blob
-    FROM
-    	req_doc
-)
-select * from req_doc;\q
-INSERT INTO mycore.title (
-	blob,
-	title
-  ) SELECT
-	blob,
-  	title
-      FROM
-      	titles
-    ON CONFLICT (blob) DO 
-      UPDATE SET
-      	title = EXCLUDED.title
+INSERT INTO load_request
+  VALUES (:'j'::jsonb)
 ;
+\set j ''
+
+CREATE TEMP TABLE load_title
+(
+	blob		udig,
+	title		text,
+	has_dup		bool default false
+);
+
+INSERT INTO load_title(blob, title)
+  WITH req_doc AS (
+    SELECT
+  	jsonb_array_elements(doc->'upsert'->'titles') AS doc
+      FROM
+    	load_request
+  ) SELECT
+  	(doc->>'blob')::udig,
+  	doc->>'title' AS title
+      FROM
+    	req_doc
+      WHERE
+      	(doc->>'blob') ~ '^[a-z][a-z0-9_]{0,7}:[[:graph:]]{32,128}$'
+;
+
+CREATE INDEX ON load_title(blob); 
+ANALYZE load_title;
+
+UPDATE load_title lt1
+  SET
+  	has_dup = true
+  WHERE EXISTS (
+    SELECT
+    	true
+      FROM
+      	load_title lt2
+      WHERE
+      		lt2.blob = lt1.blob
+		AND
+		lt2.title != lt1.title
+    )
+;
+
+INSERT INTO blob
+  SELECT
+  	blob
+    FROM
+    	load_title
+    WHERE
+    	NOT has_dup
+    ON CONFLICT
+    	DO NOTHING
+;
+
+INSERT INTO title(blob, title)
+  SELECT DISTINCT
+  	blob,
+	title
+    FROM
+    	load_title
+    WHERE
+    	NOT has_dup
+  ON CONFLICT (blob) DO UPDATE
+    SET
+    	title = EXCLUDED.title
+    WHERE
+    	title.title != EXCLUDED.title
+;
+  	
+select has_dup, count(*) from load_title group by 1;
+
+END TRANSACTION;
